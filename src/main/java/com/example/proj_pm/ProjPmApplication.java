3f7621fe88fb0238cfe04046885c546be509c4c2
Jiarui Shu <shujr0029@gmail.com>
2026-08-16 19:24:39 -0700

    inti

---
+++ src/main/java/com/example/proj_pm/ProjPmApplication.java
@@ -0,0 +1,13 @@
+package com.example.proj_pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjPmApplication.class, args);
+	}
+
+}
